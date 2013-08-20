f0c5c4892f861e78437f8f87245d9e5c3a2a3324
FelipeCabral <felipecv7@gmail.com>
2013-08-19 21:43:02 -0300

    upload project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tir::Application.config.secret_token = '6aaf8867bab2f42520b9d5cc84fab9de6ba94e058b0a53d2d560333801e273fd12190a3efbb6e896e027f350e77b105caa149926ab4b9df188b518d59c501779'
